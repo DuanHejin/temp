f6429ddb48dcf34e4a135ac91caf8433495d9f9a
duanhejin <duanhejin@zhoupudata.com>
2022-06-15 14:17:48 +0800

    promiseAllSettled

---
+++ src/node/promiseAllSettled/index.js
@@ -0,0 +1,22 @@
+
+var assert = require('assert');
+var allSettled = require('promise.allsettled');
+
+var resolved = Promise.resolve(42);
+var rejected = Promise.reject(-1);
+
+allSettled([resolved, rejected]).then(function (results) {
+	assert.deepEqual(results, [
+		{ status: 'fulfilled', value: 42 },
+		{ status: 'rejected', reason: -1 }
+	]);
+});
+
+allSettled.shim(); // will be a no-op if not needed
+
+Promise.allSettled([resolved, rejected]).then(function (results) {
+	assert.deepEqual(results, [
+		{ status: 'fulfilled', value: 42 },
+		{ status: 'rejected', reason: -1 }
+	]);
+});
